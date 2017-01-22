dd91d8ba3dd411a7ce034f44cfa284d8bfdd5f44
stewart leach <stewart.leach@originenergy.com.au>
2017-01-22 15:23:01 +1100

    adding comments around usage for module vs single component

--- src/app/components/components.js
+++ src/app/components/components.js
@@ -7,16 +7,17 @@ import todoFooterComponent from './todoFooter.component';
 import todoBatchToggle from './todoBatchToggle.component';
 import todoListFilter from './todoListFilter.component';
 
-import sampleComponent from './sampleComponent/sampleComponent'
+import sampleModule from './sampleComponent/sampleComponent' //defined as a module. Can contain multiple components
+import sampleComponent from './sampleComponent/sampleComponent.component' //singular component
 
 export default angular
   .module('app.components', [
     todoTextInput.name,
-    sampleComponent.name
+    sampleModule.name //include as a module. Useful for including multiple components into a module
   ])
   .component('todoItem', todoItemComponent)
   .component('todoFooter', todoFooterComponent)
   .component('todoBatchToggle', todoBatchToggle)
   .component('todoListFilter', todoListFilter)
-  //.component('sampleComponent', sampleComponent)
+  //.component('sampleComponent', sampleComponent) //include as single component into the module
 ;
