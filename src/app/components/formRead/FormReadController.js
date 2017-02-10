1c7add40f259df92e6f5a501bbc133e3082cadf8
stewart leach <stewart.leach@originenergy.com.au>
2017-02-10 20:28:04 +1100

    refactoring formRead

--- src/app/components/formRead/FormReadController.js
+++ src/app/components/formRead/FormReadController.js
@@ -1,28 +1,11 @@
-(function() {
-  'use strict';
+import template from "./formRead.html";
 
-  angular
-    .module('app.user.formReadController')
-    .controller('FormReadController', Controller);
-
-  Controller.$inject = [
-    '$routeParams',
-    '$http',
-    '$cookies',
-    '$mdDialog',
-    'loomApi'
-  ];
-
-  //example child router. Component folder structure stays flat
-  Controller.$routeConfig = [
-    { path: '/create', component: 'testCreate' }
-  ];
-
-  function Controller($routeParams, $http, $cookies, $mdDialog, loomApi) {
+class Controller{
+  constructor($cookies, $mdDialog, loomApi){
+    "ngInject";
     console.log("FormReadController instantiated");
     console.log("jobDetailFormId passed in from dialog controller:" + this.jobDetailFormId);
 
-    this.formId = $routeParams.id == null ? this.jobDetailFormId : $routeParams.id;
     this.article = {"skills": []}; //Need to initialise for md-chips, otherwise an exception is thrown
     this.payFrequencyOptions = [
       {id: "Permanent", value: "annual salary"},
@@ -38,21 +21,29 @@
       console.log(result);
       this.article = result;
     }));
-    
-    //make this a reusable service
-    var checkAuth = function(username, password){ //ToDo: does this need refactoring to use local storage rather than cookie?
-      var authCookie = $cookies.get("writeon.authtoken"); //put cookie name into config var
-      return typeof authCookie != 'undefined' ? authCookie : loomApi.User.signInUser(username, password).then(angular.bind(this, function(result){
-        $cookies.put("writeon.authtoken", result.token);
-        this.authToken = result.token; //required angular.bind to enable setting of scope variable within promise
-      }));
-    };
-
-    this.cancelDialog = function() {
-      console.log("cancelDialog");
-      $mdDialog.hide();
-    }
-    
   }
 
-})();
+  $routerOnActivate(next, previous){
+    this.formId = next.params.id == null ? this.jobDetailFormId : next.params.id;
+  }
+
+  //make this a reusable service
+  checkAuth(username, password){ //ToDo: does this need refactoring to use local storage rather than cookie?
+    var authCookie = $cookies.get("writeon.authtoken"); //put cookie name into config var
+    return typeof authCookie != 'undefined' ? authCookie : loomApi.User.signInUser(username, password).then(angular.bind(this, function(result){
+      $cookies.put("writeon.authtoken", result.token);
+      this.authToken = result.token; //required angular.bind to enable setting of scope variable within promise
+    }));
+  };
+
+  cancelDialog(){
+    console.log("cancelDialog");
+    $mdDialog.hide();
+  }
+}
+
+export default {
+  controller: Controller,
+  controllerAs: 'formRead',
+  template: template
+}
--- src/app/components/formRead/formRead.js
+++ src/app/components/formRead/formRead.js
@@ -1,2 +1,7 @@
-angular
-  .module('app.user.formReadController', []);
+import angular from 'angular';
+
+import Controller from './FormReadController';
+
+export default angular
+  .module('app.user.formReadController', [])
+  .component('formRead', Controller);
