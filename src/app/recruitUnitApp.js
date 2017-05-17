31ea5f5df9f0d8a6fb211f7a943f161d75aa4c3a
stewart leach <stewartleach@Naya-Las-MacBook-Pro.local>
2017-05-17 21:24:53 +1000

    moving dependencies from dev to standard to see if it runs on DO server

--- src/app/recruitUnitApp.js
+++ src/app/recruitUnitApp.js
@@ -16,7 +16,6 @@ import Services from './services/services';
 import Components from './components/components';
 import RootComponent from './root.component';
 import unitTestFormTemplate from './unitTestFormDialog.html';
-import './recruitunit.config';
 
 import '../css/global.css';
 
